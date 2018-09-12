d8288380ae3248b02f82912e0a7aa84c5459cdda
Keks <keks@htmlacademy.ru>
2018-09-12 17:32:28 +0300

    :hatching_chick:

---
+++ data.php
@@ -0,0 +1,8 @@
+<?php
+// ставки пользователей, которыми надо заполнить таблицу
+$bets = [
+    ['name' => 'Иван', 'price' => 11500, 'ts' => strtotime('-' . rand(1, 50) .' minute')],
+    ['name' => 'Константин', 'price' => 11000, 'ts' => strtotime('-' . rand(1, 18) .' hour')],
+    ['name' => 'Евгений', 'price' => 10500, 'ts' => strtotime('-' . rand(25, 50) .' hour')],
+    ['name' => 'Семён', 'price' => 10000, 'ts' => strtotime('last week')]
+];
