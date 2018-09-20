d2caaeeca65d12e6a0aa64512d31b23e437562ed
Ivan <ivanbdn@yandex.ru>
2018-09-20 17:18:10 +0300

    Функция strip_tags заменена на htmlspecialchars

--- templates/index.php
+++ templates/index.php
@@ -21,15 +21,15 @@
         <?php foreach ($adverts as $key => $val): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?=strip_tags($val['url'])?>" width="350" height="260" alt="">
+                    <img src="<?=htmlspecialchars($val['url'])?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=strip_tags ($val['category'])?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=strip_tags ($val['name'])?></a></h3>
+                    <span class="lot__category"><?=htmlspecialchars($val['category'])?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($val['name'])?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=ruble_display(strip_tags($val['price']))?> </span>
+                            <span class="lot__cost"><?=ruble_display(htmlspecialchars($val['price']))?> </span>
                         </div>
                         <div class="lot__timer timer">
 
