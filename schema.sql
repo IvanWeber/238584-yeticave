d548ced73d189a026133864b15606a2f51158bcc
Ivan <ivanbdn@yandex.ru>
2018-09-23 07:06:08 +0300

    Создание схемы БД личного проекта

---
+++ schema.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE Yeticave
+	DEFAULT CHARACTER SET utf8
+	DEFAULT COLLATE utf8_general_ci;
+	
+CREATE TABLE category (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name CHAR(64)
+);
+
+CREATE TABLE lot (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	creation_date  DATE,
+	name CHAR(128),
+	discription CHAR(255),
+	image BLOB,
+	start_price MEDIUMINT,
+	end_date DATE,
+	rate_step SMALLINT
+);
+
+CREATE TABLE rate (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	date DATE,
+	sum MEDIUMINT
+);
+
+CREATE TABLE users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	registration_date DATETIME,
+	email CHAR(128),
+	name CHAR(128),
+	password CHAR(64),
+	avatar BLOB,
+	contacts CHAR(255)
+);
+
+	
+	
