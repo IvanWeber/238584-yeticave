8b10718e32c1da1d423b6513a9c89f86f2c19d11
Ivan <ivanbdn@yandex.ru>
2018-09-25 07:35:56 +0300

    Установлены связи между таблицами

--- schema.sql
+++ schema.sql
@@ -2,26 +2,12 @@ CREATE DATABASE Yeticave
 	DEFAULT CHARACTER SET utf8
 	DEFAULT COLLATE utf8_general_ci;
 	
-CREATE TABLE category (
+	
+USE Yeticave;
+	
+CREATE TABLE categories (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	name CHAR(64)
-);
-
-CREATE TABLE lot (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	creation_date  DATE,
-	name CHAR(128),
-	discription CHAR(255),
-	image CHAR(128),
-	start_price MEDIUMINT,
-	end_date DATE,
-	rate_step SMALLINT
-);
-
-CREATE TABLE rate (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	date DATE,
-	sum MEDIUMINT
 );
 
 CREATE TABLE users (
@@ -34,7 +20,34 @@ CREATE TABLE users (
 	contacts CHAR(255)
 );
 
-USE Yeticave;
+CREATE TABLE lots (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	creation_date  DATE,
+	name CHAR(128),
+	discription CHAR(255),
+	image CHAR(128),
+	start_price MEDIUMINT,
+	end_date DATE,
+	rate_step SMALLINT,
+	categories_id INT,
+	user_id INT,
+	winner_id INT,
+	FOREIGN KEY (categories_id) REFERENCES categories(id),
+	FOREIGN KEY (user_id) REFERENCES users(id),
+	FOREIGN KEY (winner_id) REFERENCES users(id)
+);
+
+CREATE TABLE rate (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	date DATE,
+	sum MEDIUMINT,
+	user_id INT,
+	lot_id INT,
+	FOREIGN KEY (user_id) REFERENCES users(id),
+	FOREIGN KEY (lot_id) REFERENCES lots(id)
+);
+
+
 
 	
 	
